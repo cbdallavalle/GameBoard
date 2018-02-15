3e35c2213c91c3f6f8c3d36e5c031be24fead226
Casey Dallavalle <cbdallavalle@gmail.com>
2018-02-15 11:40:40 -0700

    Add proxy to fetch in order to work around CORS. Then the response is grabbing the text and then calling a parse function to convert the XML from a string to DOM elements

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import $ from 'jquery';
+import './Search.css';
 
 export class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       results: [],
+      search: this.props.search
     }
   }
 
@@ -13,18 +16,40 @@ export class Search extends Component {
     this.triggerSearch('arkhaM Horror');
   }
 
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   triggerSearch = async () => {
-    const search = this.props.search.toLowerCase().split(' ').join('%20');
+    const search = this.state.search.toLowerCase().split(' ').join('%20');
     console.log(search)
-    // const response = await fetch(`https://www.boardgamegeek.com/xmlapi/search?search=${search}`);
-    // const games = await response.json();
+    // const response = await fetch(`https://cors-anywhere.herokuapp.com/www.boardgamegeek.com/xmlapi2/search?query=${search}`);
+    // const response = await fetch(`https://cors-anywhere.herokuapp.com/www.boardgamegeek.com/xmlapi2/search?query=pandemic`);
+    // const responseText = await response.text();
+    // const games = new window.DOMParser().parseFromString(responseText, "text/xml")
     // console.log(games)
+  // "proxy": "https://cors-anywhere.herokuapp.com/"
+    
+    // console.log(JSON.stringify(games))
   }
 
   render() {
     return (
       <section className='Search'>
-        hiasdfa
+        <h1>Search for Games</h1>
+        <form>
+          <input 
+            type="text"
+            name="search"
+            placeholder="Search for board games"
+            value={this.state.search}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Search</button>
+        </form>
       </section>
     )
   }
